c8635c46df49b1227b19759a6b9fef22a301bd6b
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-04-24 19:34:40 +0500

    Three JS installation and Configuration

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -1,10 +1,103 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import ModelView from "./ModelView";
+import { use, useEffect, useRef, useState } from "react";
+import { yellowImg } from "../utils";
+import * as THREE from "three";
+import { View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { models } from "../Constants";
+import { div } from "three/tsl";
+
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in natural Titanium",
+    colors: ["#8F8A81", "#FFE7B9", "#6F6C64"],
+    image: yellowImg,
+  });
+
+  // This refer to our camera control
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  //This refer to our model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // This refers to our model rotation value
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  useGSAP(() => {
+    gsap.to("#heading", {
+      scrollTrigger: {
+        trigger: "#heading",
+      },
+      y: 0,
+      opacity: 1,
+      duration: 0.5,
+    });
+  }, []);
   return (
     <section className="common-padding">
       <div className="screen-max-width">
         <h1 id="heading" className="section-heading">
           Take a closer look
         </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm text-center mb-5 font-light">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
