3a08cad9a2061577b81a2a4c275d663cc1b5c003
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-04-27 02:19:16 +0500

    Additional Features and Sentry Issues resolved

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,12 +1,23 @@
+import { useGSAP } from "@gsap/react";
 import { explore1Img, explore2Img, exploreVideo } from "../utils";
 import { animateWithGsap } from "../utils/animation";
 import { useRef } from "react";
 
 const Features = () => {
-  animateWithGsap("#features", { y: 0, opacity: 1 }, {start: "top 80%" });
-  animateWithGsap("#exploreVideo", { y: 0 }, { start: "top 80%" });
-    animateWithGsap(".g_grow", {scale: 1, opacity: 1, ease: 'power1' }, {scrub: 5.5});
   const videoRef = useRef();
+
+  animateWithGsap("#features", { y: 0, opacity: 1 }, { start: "top 85%" });
+  animateWithGsap("#exploreVideo", { y: 0 }, { start: "top 85%" });
+  animateWithGsap(".g_grow",
+    { scale: 1, opacity: 1, ease: "power1" },
+    { scrub: 5.5, start: "top 85%" }
+  );
+  animateWithGsap(".g_text", {
+    y: 0,
+    opacity: 1,
+    ease: "power2.inout",
+    duration: 1,
+  });
 
   return (
     <div className="h-full common-padding bg-zinc relative overflow-hidden">
@@ -38,22 +49,40 @@ const Features = () => {
               </video>
             </div>
             <div className="flex flex-col w-full relative">
-                <div className="feature-video-container">
-                    <div className="overflow-hidden flex-1 h-[50vh]">
-                        <img
-                            src={explore1Img}
-                            alt="titanium"
-                            className="featuere-video g_grow"
-                        />
-                    </div>
-                    <div className="overflow-hidden flex-1 h-[50vh]">
-                        <img
-                            src={explore2Img}
-                            alt="titanium"
-                            className="featuere-video g_grow"
-                        />
-                    </div>
+              <div className="feature-video-container">
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
                 </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    src={explore2Img}
+                    alt="titanium 2"
+                    className="feature-video g_grow"
+                  />
+                </div>
+              </div>
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 pro is {""}
+                    <span className="text-white">
+                      the finest iPhone to feature an aerospace-grade tatinium
+                      design
+                    </span>
+                    using the same alloy that space crafts use for missions to
+                    Mars.
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of any metal, making these our {""}
+                    <span className="text-white">
+                      lighest pro model ever
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
